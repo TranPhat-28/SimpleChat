f6dfb7dba3ea77a6980e7409f246aa3cf6036fbf
TranPhat-28 <62002249+TranPhat-28@users.noreply.github.com>
2022-08-22 12:48:47 +0700

    Add files via upload

---
+++ Client/ClientForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace ChatClient
+{
+    public partial class ClientForm : Form
+    {
+        string displayname = "";
+        TcpClient client = null;
+        NetworkStream stream = null;
+        StreamWriter sw = null;
+
+        //AddTextCallBack
+        private delegate void AddTextCallback(string input);
+
+        public ClientForm()
+        {
+            InitializeComponent();
+            displayname = textBoxdisplayname.Text;
+        }
+
+        // ------------[Connect]------------------
+        // Nhấn nút Connect tạo kết nối đến Server
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // Xóa richTextBox
+            richTextBox1.Clear();
+
+            // Disable textBox ip port và nút connect, nút apply
+            textBoxip.Enabled = false;
+            textBoxport.Enabled = false;
+            btnConnect.Enabled = false;
+            buttonDis.Enabled = true;
+            textBoxdisplayname.Enabled = false;
+            buttonSetDisplayName.Enabled = false;
+
+            AddText("Connected to server");
+
+            client = new TcpClient();
+            client.Connect(IPAddress.Parse(textBoxip.Text), int.Parse(textBoxport.Text));
+            stream = client.GetStream();
+            sw = new StreamWriter(stream);
+
+            Thread l = new Thread(new ThreadStart(listen));
+            l.Start();
+        }
+
+        //--------[Send]-----------
+        //Nhấn nút Send để gửi tin
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string msg = displayname + ": " + textBox1.Text;
+            sw.WriteLine(msg);
+            sw.Flush();
+            textBox1.Clear();
+        }
+
+        //Hàm xử lí việc gọi richTextBox từ Thread khác
+        private void AddText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the 
+            // calling thread to the thread ID of the creating thread. 
+            // If these threads are different, it returns true. 
+            if (this.richTextBox1.InvokeRequired)
+            {
+                AddTextCallback d = new AddTextCallback(AddText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.richTextBox1.AppendText(text + '\n');
+            }
+        }
+
+        // Ngắt kết nối
+        private void buttonDis_Click(object sender, EventArgs e)
+        {
+            // Enable textBox ip port và nút connect, nút apply
+            textBoxip.Enabled = true;
+            textBoxport.Enabled = true;
+            btnConnect.Enabled = true;
+            buttonDis.Enabled = false;
+            textBoxdisplayname.Enabled = true;
+            buttonSetDisplayName.Enabled = true;
+
+            // Hủy stream và đóng client
+            stream.Close();
+            client.Close();
+        }
+
+        // Hàm nghe phản hồi lại từ Server
+        private void listen()
+        {
+            StreamReader sr = new StreamReader(stream);
+            string output = "";
+            while (output != null)
+            {
+                try
+                {
+                    output = sr.ReadLine();
+                    if (output == null) break;
+                    AddText(output);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            AddText("Disconnected from Server!");
+        }
+
+        // Đổi tên hiển thị
+        private void buttonSetDisplayName_Click(object sender, EventArgs e)
+        {
+            displayname = textBoxdisplayname.Text;
+            MessageBox.Show("Display name changed!", "Success");
+        }
+    }
+}
---
+++ Server/ServerForm.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace ChatServer
+{
+    public partial class ServerForm : Form
+    {
+        //Biến toàn cục
+        TcpListener listener = null;
+        Socket sock = null;
+        NetworkStream stream = null;
+        Boolean isRunning = false;
+        string newMsg = "";
+
+        List<Thread> clientHandlerList = new List<Thread>();
+        List<StreamReader> readerList = new List<StreamReader>();
+        List<NetworkStream> streamList = new List<NetworkStream>();
+        List<int> clientID = new List<int>();
+        int count = 0;
+
+        // AddTextCallBack
+        private delegate void AddTextCallback(string input);
+
+        public ServerForm()
+        {
+            InitializeComponent();
+        }
+
+        //----[Start Server]------
+        //Khi nhất nút Start Server
+        private void btnStartServer_Click(object sender, EventArgs args)
+        {
+            // Disable nút connect và textBox
+            textBoxip.Enabled = false;
+            textBoxport.Enabled = false;
+            btnStartServer.Enabled = false;
+            buttonStop.Enabled = true;
+
+            isRunning = true;
+            AddText("Server started...");
+
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 10000);
+            Thread a = new Thread(new ThreadStart(serverHandler));
+            a.Start();
+        }
+
+        //Thread dùng để lắng nghe kết nối mới từ client
+        private void serverHandler()
+        {
+            while (true)
+            {
+                try
+                {
+                    listener.Start();
+
+                    sock = listener.AcceptSocket();
+                    AddText("New client connected from: " + sock.RemoteEndPoint.ToString());
+
+                    clientHandlerList.Add(new Thread(new ThreadStart(clientHandler)));
+                    clientHandlerList[clientHandlerList.Count - 1].Start();
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            AddText("Server stopped.");
+        }
+
+        //Xử lí việc lắng nghe tin nhắn từ Client
+        private void clientHandler()
+        {
+            int id = count;
+            count++;
+
+            streamList.Add(new NetworkStream(sock));
+            readerList.Add(new StreamReader(streamList[id]));
+
+            string output = "";
+            while (output != null)
+            {
+                try
+                {
+                    output = readerList[id].ReadLine();
+                    newMsg = output;
+                    if (output == null) break;
+                    AddText(output);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            AddText("Client disconnected!");
+        }
+
+        //----[Stop Server]------
+        //Khi nhất nút Stop Server
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            // Enable nút connect và textBox
+            textBoxip.Enabled = true;
+            textBoxport.Enabled = true;
+            btnStartServer.Enabled = true;
+            buttonStop.Enabled = false;
+
+            sock = null;
+            listener.Stop();
+
+            isRunning = false;
+            // Đóng tất cả các Stream và ngừng listener
+            for (int i = 0; i < streamList.Count; i++)
+            {
+                streamList[i].Dispose();
+                streamList[i].Close();
+            }
+        }
+
+        //Hàm xử lí việc gọi listView từ Thread khác
+        private void AddText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the 
+            // calling thread to the thread ID of the creating thread. 
+            // If these threads are different, it returns true. 
+            if (this.richTextBox1.InvokeRequired)
+            {
+                AddTextCallback d = new AddTextCallback(AddText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.richTextBox1.AppendText(text + '\n');
+            }
+        }
+
+        //Nếu có nội dung mới từ Client, richTextBox sẽ cập nhật nội dung
+        //Sau đó gửi lại 
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < streamList.Count; i++)
+            {
+                try
+                {
+                    StreamWriter sw = new StreamWriter(streamList[i]);
+                    sw.WriteLine(newMsg);
+                    sw.Flush();
+                }
+                catch
+                {
+                    continue;
+                }
+            }    
+        }
+    }
+}
